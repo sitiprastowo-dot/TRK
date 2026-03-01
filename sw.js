edfecd2e4a565f49fb25ce1ad85caa3d7627c3c1
sitiprastowo-dot <siti.prastowo@gmail.com>
2026-03-01 21:02:32 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'trk-pos-v32'; 
+const CACHE_NAME = 'trk-pos-v33'; 
 const ASSETS = [
   './',
   './index.html',
@@ -7,31 +7,6 @@ const ASSETS = [
   'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;700;800&family=Space+Mono:wght@400;700&display=swap'
 ];
 
-// Install: Cache files
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
-});
-
-// Activate: Clean up old caches
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Fetch: Serve from cache if offline
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
-});
+self.addEventListener('install', (e) => { e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))); });
+self.addEventListener('activate', (e) => { e.waitUntil(caches.keys().then((keys) => { return Promise.all(keys.map((key) => { if (key !== CACHE_NAME) return caches.delete(key); })); })); });
+self.addEventListener('fetch', (e) => { e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request))); });
