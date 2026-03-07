c059f02fdb7de9bbce2df63418f743a5d8a42c4b
sitiprastowo-dot <siti.prastowo@gmail.com>
2026-03-07 12:13:21 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tr-pos-v39'; 
+const CACHE_NAME = 'trk-pos-v39'; 
 const ASSETS = [
   './',
   './index.html',
@@ -10,3 +10,4 @@ const ASSETS = [
 self.addEventListener('install', (e) => { e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))); });
 self.addEventListener('activate', (e) => { e.waitUntil(caches.keys().then((keys) => { return Promise.all(keys.map((key) => { if (key !== CACHE_NAME) return caches.delete(key); })); })); });
 self.addEventListener('fetch', (e) => { e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request))); });
+
